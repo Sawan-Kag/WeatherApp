e8fe427020b28c9025e0d87311637e9df97e39b5
sawankag <sawan.kag@nagarro.com>
2023-12-18 18:44:44 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  XRapidAPIBaseUrl: 'https://open-weather13.p.rapidapi.com/city',
+  XRapidAPIKeyValue: '7d85a6aec7msh6de6055794e7f63p1bb1ccjsn68a27487dadf',
+  XRapidAPIKeyName: 'X-RapidAPI-Key',
+
+  XRapidAPIHostName: 'X-RapidAPI-Host',
+  XRapidAPIHostValue: 'open-weather13.p.rapidapi.com',
+};
